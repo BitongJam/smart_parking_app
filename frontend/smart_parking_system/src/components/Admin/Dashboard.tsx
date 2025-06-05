710c54bd98c08fdb2bb8745cba1d6afff6b539f2
BItongJam <jamesortizalter@gmail.com>
2025-06-06 03:05:05 +0800

    [ADD] AdminParkingReservation component on Dashbaord

--- frontend/smart_parking_system/src/components/Admin/Dashboard.tsx
+++ frontend/smart_parking_system/src/components/Admin/Dashboard.tsx
@@ -1,8 +1,12 @@
+import AdminParkingReservation from "./AdminParkingReservation";
+
 function Dashboard(){
     return <>
         <div className="container">
 
             <h1>Admin Dashboard</h1>
+
+            <AdminParkingReservation />
         </div>
     </>
 }   
