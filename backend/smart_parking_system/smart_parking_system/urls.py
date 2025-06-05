f5eb3909781330cdd5a302f850d9a0e38d835d5b
BItongJam <jamesortizalter@gmail.com>
2025-06-06 03:25:18 +0800

    [ADD] add urls of app notification on root project urls

--- backend/smart_parking_system/smart_parking_system/urls.py
+++ backend/smart_parking_system/smart_parking_system/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/",include('api.urls')),
-    path("api/user/",include('users.urls'))
+    path("api/user/",include('users.urls')),
+    path("api/notification/",include('notification.urls'))
 ]
