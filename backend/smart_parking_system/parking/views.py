540c9f8477f9f214ae9109affe567c57e1a44138
BItongJam <jamesortizalter@gmail.com>
2025-06-06 03:14:02 +0800

    [ADD] ParkingResevationViewSet: new function -user_reservation_history -user_active_reservation -user_draft_reservation

--- backend/smart_parking_system/parking/views.py
+++ backend/smart_parking_system/parking/views.py
@@ -19,3 +19,25 @@ class ParkingResevationViewSet(viewsets.ModelViewSet):
         # get the current user login the auto set it when reservation
         user = self.request.user
         serializer.save(user_id=user)
+
+    @action(detail=False, methods=['get'])
+    def user_reservation_history(self,request):
+        #get the user reqeuster detail
+        user = request.user
+        history = self.queryset.filter(user_id = user.id)
+        serializer = self.get_serializer(history,many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods =['get'])
+    def user_active_reservations(self,request):
+        user = request.user
+        reservations = self.queryset.filter(user_id = user.id, state="active")
+        serializer = self.get_serializer(reservations,many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods =['get'])
+    def user_draft_reservations(self,request):
+        reservations= self.queryset.filter(state='draft')
+        serializer = self.get_serializer(reservations,many=True)
+        return Response(serializer.data)
+        
