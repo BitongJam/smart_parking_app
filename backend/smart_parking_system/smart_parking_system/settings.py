ee109dedb83bb46820ddb1c9528d93a8d50fbb1e
BItongJam <jamesortizalter@gmail.com>
2025-06-13 13:32:22 +0800

    [IMP] change sqlite to postgresql setup

--- backend/smart_parking_system/smart_parking_system/settings.py
+++ backend/smart_parking_system/smart_parking_system/settings.py
@@ -88,9 +88,13 @@ WSGI_APPLICATION = 'smart_parking_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "smart_parking_app_db",
+        "USER": "spa",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5434",
     }
 }
 
