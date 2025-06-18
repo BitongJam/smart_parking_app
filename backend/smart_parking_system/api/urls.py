13ab1f49d628f350a0d18ba33e0f593fdd14a9df
James Ortiz <42773754+BitongJam@users.noreply.github.com>
2025-06-18 23:14:22 +0800

    [ADD] this view function on api ..will check if the backend is available to be connected or down (#4)

--- backend/smart_parking_system/api/urls.py
+++ backend/smart_parking_system/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import  path,include
-
+from .views import ping
+from users.views import login,user_profile
 
 urlpatterns  = [
-    path('parking/',include('parking.urls'))
+    path('parking/',include('parking.urls')),
+    path('ping/',ping),
+    path('login-user/',login),
+    path('user-profile/',user_profile)
 ]
